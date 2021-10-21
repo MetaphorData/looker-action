9ef0d50e12b73b204e5c1546be993f1adbcd2fe3
Yi Wang <yi@metaphor.io>
2021-10-20 21:01:38 -0700

    change the param order in yaml config

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ base_url: "$1"
 client_id: "$2"
 client_secret: "$3"
 lookml_dir: "/github/workspace/$4"
+projectSourceUrl: "$5"
 output:
   file:
-    path: "$5"
-projectSourceUrl: "$6"
+    path: "$6"
 EOF
 
 python -m metaphor.looker looker.yaml
